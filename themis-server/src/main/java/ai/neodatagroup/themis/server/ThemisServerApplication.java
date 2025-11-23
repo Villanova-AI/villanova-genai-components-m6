eeb3e281d06dd41f930b1e218e8a91e4cc04c865
Calogero Zarba <calogero.zarba@neodatagroup.ai>
2025-11-23 15:07:02 +0100

    Do not generate OpenApiGeneratorApplication.java

---
+++ themis-server/src/main/java/ai/neodatagroup/themis/server/ThemisServerApplication.java
@@ -0,0 +1,11 @@
+package ai.neodatagroup.themis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemisServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThemisServerApplication.class);
+    }
+}
